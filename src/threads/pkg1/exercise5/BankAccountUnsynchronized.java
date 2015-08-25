55487b2dee4b439bf9ba210251950cf0cd3be6f8
Alexander Steen <ale.steen@hotmail.com>
2015-08-25 20:50:46 +0200

    Completed the exercise

--- src/threads/pkg1/exercise5/BankAccountUnsynchronized.java
+++ src/threads/pkg1/exercise5/BankAccountUnsynchronized.java
@@ -6,26 +6,58 @@ import java.util.concurrent.locks.ReentrantLock;
 // NOT THREADSAFE
 public class BankAccountUnsynchronized {
 
-  private double balance;
-  private Lock lock = new ReentrantLock();
+    private double balance;
+    private Lock lock = new ReentrantLock();
 
-  public BankAccountUnsynchronized() {
-    balance = 0;
-  }
+    public BankAccountUnsynchronized() {
+        balance = 0;
+    }
 
-  public synchronized void deposit(double amount) {
+//  public synchronized void deposit(double amount) {
+////    double newBalance = balance + amount;
+////    balance = newBalance;  
+//    balance += amount;
+//   }
+//
+//  public synchronized void withdraw(double amount) {
+////    double newBalance = balance - amount;
+////    balance = newBalance;
+//     balance -= amount;
+//   }
+//
+//  public synchronized double getBalance() {
+//    return balance;
+//  }
+    public void deposit(double amount) {
 //    double newBalance = balance + amount;
 //    balance = newBalance;  
-    balance += amount;
-   }
+        lock.lock();
+        try {
+            balance += amount;
+        } finally {
+            lock.unlock();
+        }
+    }
 
-  public synchronized void withdraw(double amount) {
+    public void withdraw(double amount) {
 //    double newBalance = balance - amount;
 //    balance = newBalance;
-     balance -= amount;
-   }
+        lock.lock();
 
-  public synchronized double getBalance() {
-    return balance;
-  }
+        try {
+            balance -= amount;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public double getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
+
+    }
 }
