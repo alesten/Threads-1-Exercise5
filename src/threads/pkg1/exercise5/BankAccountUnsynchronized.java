cb2c5c2ce4adb01ed1377698a977ce0ca80e3937
Alexander Steen <AlexanderSteen@Alexanders-MacBook-Pro.local>
2015-08-25 11:59:41 +0200

    Added exercise files

---
+++ src/threads/pkg1/exercise5/BankAccountUnsynchronized.java
@@ -0,0 +1,31 @@
+package threads.pkg1.exercise5;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+// NOT THREADSAFE
+public class BankAccountUnsynchronized {
+
+  private double balance;
+  private Lock lock = new ReentrantLock();
+
+  public BankAccountUnsynchronized() {
+    balance = 0;
+  }
+
+  public void deposit(double amount) {
+//    double newBalance = balance + amount;
+//    balance = newBalance;  
+    balance += amount;
+   }
+
+  public void withdraw(double amount) {
+//    double newBalance = balance - amount;
+//    balance = newBalance;
+     balance -= amount;
+   }
+
+  public double getBalance() {
+    return balance;
+  }
+}
